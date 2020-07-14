2edf026ace3ba1c0fd636bb8e0188219b84f0a04
Watson <dunwatso@38f9d3c98a96.ant.amazon.com>
2020-07-14 23:26:43 +0100

    menu final

--- src/frontend/layouts/Default.vue
+++ src/frontend/layouts/Default.vue
@@ -50,12 +50,12 @@
           <q-item-main label="History" sublabel="Payment and Fuel History" />
         </q-item>
         <q-item :to="{ name: 'bookings' }" exact>
-          <q-item-side icon="help_outline" />
-          <q-item-main label="Help - FAQ" sublabel="Frequently Asked Questions" />
-        </q-item>
-        <q-item :to="{ name: 'bookings' }" exact>
           <q-item-side icon="fact_check" />
           <q-item-main label="Legal and Privacy" sublabel="Terms and Conditions" />
+        </q-item>
+        <q-item :to="{ name: 'bookings' }" exact>
+          <q-item-side icon="help_outline" />
+          <q-item-main label="Help - FAQ" sublabel="Frequently Asked Questions" />
         </q-item>
       </q-list>
     </q-layout-drawer>
