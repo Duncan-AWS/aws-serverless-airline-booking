b5b4884d2c94ac98f481721254783a1ba83917da
Watson <dunwatso@38f9d3c98a96.ant.amazon.com>
2020-07-14 22:31:27 +0100

    typo2

--- src/frontend/layouts/Default.vue
+++ src/frontend/layouts/Default.vue
@@ -54,10 +54,9 @@
           <q-item-main label="Help - FAQ" sublabel="Frequently Asked Questions" />
         </q-item>
         <q-item :to="{ name: 'bookings' }" exact>
-          <q-item-side icon="certificate_outline" />
+          <q-item-side icon="help_outline" />
           <q-item-main label="Legal and Privacy" sublabel="Terms and Conditions" />
         </q-item>
-
       </q-list>
     </q-layout-drawer>
 
