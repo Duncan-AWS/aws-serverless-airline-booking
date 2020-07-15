d9a0ebdcc117f3f56dae8e0e507d9ea3bb358ab7
Watson <dunwatso@38f9d3c98a96.ant.amazon.com>
2020-07-15 14:44:56 +0100

    vehicles

--- src/frontend/layouts/Default.vue
+++ src/frontend/layouts/Default.vue
@@ -45,7 +45,7 @@
           <q-item-side icon="payment" />
           <q-item-main label="Payment Card" sublabel="Credit Card" />
         </q-item>
-        <q-item :to="{ name: 'bookings' }" exact>
+        <q-item :to="{ name: 'vehicles' }" exact>
           <q-item-side icon="local_gas_station" />
           <q-item-main label="History" sublabel="Payment and Fuel History" />
         </q-item>
