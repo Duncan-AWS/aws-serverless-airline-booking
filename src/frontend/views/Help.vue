4c1f0653d72e2a1312d3b0ac2201f37976fbfd2b
Watson <dunwatso@38f9d3c98a96.ant.amazon.com>
2020-07-16 17:11:15 +0100

    legal

--- src/frontend/views/Help.vue
+++ src/frontend/views/Help.vue
@@ -20,51 +20,25 @@
     
     <div class="search__options q-pa-sm">
       <q-card flat bordered class="my-card">
+        <q-icon name="not_listed_location" />
+        
         <q-card-section>
-          <div class="text-h6">How Does Auto Pay work?</div>
+          <div class="text-h2">How Does Auto Pay work?</div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqua.
         </q-card-section>
-
-        <q-separator inset />
-   
-        <q-card-section>
-          <q-field :value="text" bottom-slots label="How does Auto Pay by Octank work?" stack-label counter :dense="dense">
-            <template v-slot:prepend>
-              <q-icon name="not_listed_location" />
-            </template>
-
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua.</div>
-            </template>
-          </q-field>
-        </q-card-section>
       </q-card>
     </div>
 
-    <div>
-      <q-field :value="text" bottom-slots label="Do I have to go into the store?" stack-label counter :dense="dense">
-          <template v-slot:prepend>
-            <q-icon name="not_listed_location" />
-          </template>
-
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua.</div>
-          </template>
-
-          <template v-slot:append>
-            <q-icon name="close" class="cursor-pointer" />
-          </template>
-
-          <template v-slot:hint>
-            Field hint
-          </template>
-      </q-field>
+    <div class="wrapper">
+      <div class="heading">
+        <div class="q-display-1 search__headline" data-test="search-headline">
+          Legal Notices
+        </div>
+      </div>
     </div>
 
 
